ee2e72271987e927337dcd441a571a5d97329311
whj <wanghaijun.425@163.com>
2024-04-22 04:28:52 +0800

    add:flutter 网格布局 Sliver 讲解分析.

---
+++ a_flutter_learn/lib/b_flutter_learn/day_09_滚动Widget-ListView、GridView、Slivers/c_Slivers/00_Readme.dart
@@ -0,0 +1,2 @@
+/// 学习方法:
+/// ListView 和 GridView 两个类的本质是什么
---
+++ a_flutter_learn/lib/b_flutter_learn/day_09_滚动Widget-ListView、GridView、Slivers/c_Slivers/01_学习方法说明.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HYHomePage(),
+    );
+  }
+}
+
+class HYHomePage extends StatelessWidget {
+  const HYHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("基础组件"),
+      ),
+      body: const HYHomeContent(),
+    );
+  }
+}
+
+/// stful 快捷键 生成:StatefulWidget
+class HYHomeContent extends StatefulWidget {
+  const HYHomeContent({super.key});
+
+  @override
+  HYHomeContentState createState() => HYHomeContentState();
+}
+
+class HYHomeContentState extends State<HYHomeContent> {
+  @override
+  Widget build(BuildContext context) {
+    /// 学习方法看 两个类有啥区别
+    ListView? list = null;
+    GridView? gride = null;
+    /**
+     * 发现 两个类继承关系都是 一样的
+     */
+    return Container(
+      color: Colors.amber,
+    );
+  }
+}
+
+/// 1h - 44 分开始分析代码.
