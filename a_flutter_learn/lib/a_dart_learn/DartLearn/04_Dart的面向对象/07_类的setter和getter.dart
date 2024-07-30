dd78a73a7ccae39e114af54ad9c0d98de1e2ca01
whj <wanghaijun.425@163.com>
2024-07-30 11:26:31 +0800

    add:dart 类的 setter 和 getter .

--- a_flutter_learn/lib/a_dart_learn/DartLearn/04_Dart的面向对象/07_类的setter和getter.dart
+++ a_flutter_learn/lib/a_dart_learn/DartLearn/04_Dart的面向对象/07_类的setter和getter.dart
@@ -1,21 +1,35 @@
 main(List<String> args) {
   final p = Person();
 
-  // 直接访问属性
+  /// 直接访问属性
   p.name = "why";
   print(p.name);
 
-  // 通过getter和setter访问
+  /// 通过getter和setter访问
   p.setName = "lilei";
   print(p.getName);
+
+  print(p._age);
+  p.setAge = 20;
+
 }
 
 
 class Person {
-  String name;
+  String name = "";
+  /// 加了 _ 下划线 访问权限变为了  本模块中 可访问  . 其他模块私有.
+  int  _age = 0;
 
-  // setter
+  /// 比如监听属性的访问:⭐️⭐️⭐️⭐️
+  /// setter
+  set setAge(int age) {
+    this._age = age;
+  }
+  int get getAge {
+    return this._age;
+  }
+  /// 使用箭头函数 简化.
   set setName(String name) => this.name = name;
-  // getter
-  String get getName => name;
+  /// getter 不能写return 箭头函数中.
+  String get getName =>  name;
 }
