7f3b163b12f56f03562f7ad9728b910195696b36
whj <wanghaijun.425@163.com>
2024-07-30 10:21:21 +0800

    add:dart 构造函数重定向.

--- a_flutter_learn/lib/a_dart_learn/DartLearn/04_Dart的面向对象/04_重定向构造函数.dart
+++ a_flutter_learn/lib/a_dart_learn/DartLearn/04_Dart的面向对象/04_重定向构造函数.dart
@@ -1,6 +1,8 @@
 main(List<String> args) {
   var p = Person("why");
   print(p.age);
+  var p1 =  Person._internal("name", 20);
+  print(p1.name);
 }
 
 
@@ -9,8 +11,8 @@ class Person {
   int age;
 
   // Person(this.name): age = 0;
-  // 构造函数的重定向
+  /// 构造函数的重定向 ---这个构造中去调用另一个构造函数就叫构造函数的重定向.
   Person(String name): this._internal(name, 0);
-
+  /// 这个也是个构造函数-命名构造函数
   Person._internal(this.name, this.age);
 }
