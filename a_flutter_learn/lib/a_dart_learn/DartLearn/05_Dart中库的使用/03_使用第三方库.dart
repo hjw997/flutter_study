83d445f80b1944aba505351a6f447313bf76d26f
whj <wanghaijun.425@163.com>
2024-08-03 05:33:29 +0800

    add:dart 使用三方库.

--- a_flutter_learn/lib/a_dart_learn/DartLearn/05_Dart中库的使用/03_使用第三方库.dart
+++ a_flutter_learn/lib/a_dart_learn/DartLearn/05_Dart中库的使用/03_使用第三方库.dart
@@ -1,8 +1,17 @@
+
+/// 三方库都使用如下方式导入 也是可以 使用 as 起别名地哈.
 import 'package:http/http.dart' as http;
 
 main(List<String> args) async {
-  var url = 'http://123.207.32.32:8000/home/multidata';
+  ///免费数据网站:
+  ///https://jsonplaceholder.typicode.com/
+  ///
+  /// 下面是红元老师的免费服务器:
+  ///
+  var url = Uri.parse("http://123.207.32.32:8000/home/multidata");
+
   var response = await http.get(url);
+
   print('Response status: ${response.statusCode}');
   print('Response body: ${response.body}');
 }
