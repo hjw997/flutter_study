b25993a2f909ef16fa8b7b647a6298f8d29b7d2f
js-amos <js-amos@bob.com>
2024-10-26 00:52:40 +0800

    add:dart 异步 Future 的 await 和 async 的使用和练习.

--- a_flutter_learn/lib/b_flutter_learn/day_10_Dart异步操作和网络请求/05_await_async的使用.dart
+++ a_flutter_learn/lib/b_flutter_learn/day_10_Dart异步操作和网络请求/05_await_async的使用.dart
@@ -1,29 +1,39 @@
 import 'dart:io';
 
-void main(List<String> args) {
+Future<void> main(List<String> args) async {
   print("main 之前---");
-  getNetWorkData().then((value) {
-    print("then:$value" );
+
+  // var res = await Future.delayed(const Duration(seconds: 3),() {
+  //   return "data1";
+  // });
+  // print("rest--$res");
+
+
+  getNetWorkData().then((onValue){
+    print("then---$onValue");
   });
+
   print("main 之后---");
 }
 
-/**
- * 解决问题:
- * 1.await必须在 async 函数中使用
- * 2.async 返回的结果必须是 Future.
- */
+/// 解决问题: ⭐️⭐️⭐️⭐️
+/// async 和 await 其实是个语法糖:
+/// 1.await 必须在 async 函数中使用
+/// 2.async 返回的结果必须是 Future.
 Future getNetWorkData() async {
   print("getNetWorkData 函数开始---");
   //  var result = await sleep(Duration(seconds: 2));
   /// 假设 sleep 是个 耗时操作, await 会等待结果,等待获得结果后继续往下执行.
   /// 用同步的代码格式,写出异步代码.
-  await Future((){
+  var res = await Future((){
     print("sleep 之前---");
-    sleep(Duration(seconds: 2));
+    sleep(const Duration(seconds: 5));
     print("sleep 之后---");
+    return "网络数据";
   });
-  print("result---");
+  print("result---$res");
+  print("getNetWorkData 函数返回-------->");
+  // return res;
 
   /// 假设拿到了  await后面函数的 返回结果 ,然后返回
   //  return Future.value("result数据");
